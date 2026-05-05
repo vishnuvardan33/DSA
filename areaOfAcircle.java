6c1999a0f6fec09b7739b29eb354eda8bf232add
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-06 00:18:31 +0530

    add

---
+++ areaOfAcircle.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class areaOfAcircle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        double area = Math.PI * r * r;
+        System.out.println("Area of the circle = " + area);
+    }
+}
---
+++ inputTypes.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class inputTypes {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 1.next
+        //String input = sc.next();
+        //System.out.println("You entered: " + input);
+
+        // 2.nextLine
+        //String name = sc.nextLine();
+        //System.out.println("Your name is: " + name);
+        // 3.nextInt
+        //int number = sc.nextInt();
+        //System.out.println("You entered the number: " + number);
+        // 4.nextDouble
+        //double decimal = sc.nextDouble();
+        //System.out.println("You entered the decimal number: " + decimal);
+        // 5.nextBoolean
+        boolean flag = sc.nextBoolean();
+        System.out.println("You entered the boolean value: " + flag);
+
+        // 6.nextLong
+        //long bigNumber = sc.nextLong();
+        //System.out.println("You entered the long number: " + bigNumber);
+        // 7.nextFloat
+        //float floatNumber = sc.nextFloat();
+        //System.out.println("You entered the float number: " + floatNumber);
+        // 8.nextShort
+        //short shortNumber = sc.nextShort();
+        //System.out.println("You entered the short number: " + shortNumber);
+        // 9.nextByte
+        //byte byteNumber = sc.nextByte();
+        //System.out.println("You entered the byte number: " + byteNumber);
+    }
+}
