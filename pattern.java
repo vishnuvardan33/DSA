dd25ffcf1cbf842dd5085006bce83c985de89c33
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-05 19:20:31 +0530

    add

--- pattern.java
+++ pattern.java
@@ -2,12 +2,13 @@
 public class pattern {
 
     public static void main(String[] args) {
-        System.out.print("\n hello java\n");
+        System.out.print("\n hello\n java\n");
         System.out.println("");
         for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= 5 - i; j++) {
                 System.out.print("*");
             }
+
             System.out.println(); // Move to the next line after each row
         }
     }
