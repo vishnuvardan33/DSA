e32ac0cab3966a82ade9b5d3d79dc7cc4bdddd92
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-08 18:36:09 +0530

    add

---
+++ conditional-statementsQ/ternary.java
@@ -0,0 +1,10 @@
+
+public class ternary {
+
+    public static void main(String[] args) {
+        int a = 60, b = 30;
+        boolean result = (a > b) ? true : false;
+        System.out.println("Is a greater than b? " + result);
+
+    }
+}
