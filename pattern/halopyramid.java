f4de90cd49ae376bce8a99e3d7dd834ae4331b85
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-10 02:01:17 +0530

    add

--- pattern/halopyramid.java
+++ pattern/halopyramid.java
@@ -6,5 +6,19 @@ public class halopyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+
     }
 }
