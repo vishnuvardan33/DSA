99a9db6e35b4648e4b3b5e3747231785b78f8be5
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-09 23:51:29 +0530

    add

---
+++ pattern/shivaji.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class shivaji {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 2) {
+            System.out.println("prime");
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                System.out.println("not prime");
+                break;
+            } else {
+                System.out.println("prime");
+                break;
+            }
+        }
+    }
+}
