fb8d2ad946c97b7ef238bc3dd6f47bea3e68f07c
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-12 07:21:57 +0530

    add

--- pattern/shivaji.java
+++ pattern/shivaji.java
@@ -8,16 +8,17 @@ public class shivaji {
         int n = sc.nextInt();
 
         if (n == 2) {
+
             System.out.println("prime");
+            return;
         }
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 System.out.println("not prime");
-                break;
-            } else {
-                System.out.println("prime");
-                break;
+                return;
             }
         }
+        System.out.println("prime");
+
     }
 }
