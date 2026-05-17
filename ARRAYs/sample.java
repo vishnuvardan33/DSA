0ff3b1eeb3cf3b40cb6060096a4f9f6be88246ae
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-17 11:37:27 +0530

    add

--- ARRAYs/sample.java
+++ ARRAYs/sample.java
@@ -2,7 +2,7 @@
 public class sample {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 3, 3, 5};
         System.out.println("Array elements:");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
