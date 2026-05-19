e79721c9045b4445aaae2f8858cb18116bdeb331
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-19 08:03:43 +0530

    add

---
+++ loops/primeRange.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class primeRange {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the lower bound: ");
+        int lower = sc.nextInt();
+        System.out.print("Enter the upper bound: ");
+        int upper = sc.nextInt();
+
+        System.out.println("Prime numbers between " + lower + " and " + upper + ":");
+        for (int i = lower; i <= upper; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+
+}
