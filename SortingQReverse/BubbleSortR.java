234adc759b7edc73ef8d80df568c26b7163504f5
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-23 12:29:00 +0530

    add

--- SortingQReverse/BubbleSortR.java
+++ SortingQReverse/BubbleSortR.java
@@ -1,28 +1,30 @@
+
 public class BubbleSortR {
-	public static void bubbleReverse(int[] arr){
-		int n = arr.length;
-		for(int i=0; i<n-1; i++){
-			for(int j=0; j<n-i-1; j++){
-				if(arr[j] < arr[j+1]){
-					
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-			}
-		}
-	}
 
-	public static void printarr(int arr[]){
-		for(int i=0; i<arr.length; i++){
-			System.out.print(arr[i]+" ");
-		}
-		
-	}
+    public static void bubbleReverse(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
 
-	public static void main(String args[]){
-		int arr[]={2,10,8,7};
-		bubbleReverse(arr);
-		printarr(arr);
-	}
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {2, 10, 8, 7};
+        bubbleReverse(arr);
+        printarr(arr);
+    }
 }
