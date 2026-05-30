8fdbcdf4a0c7be28b2e97b09b0b92fc1d28381df
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-05-30 08:26:15 +0530

    add

---
+++ ARRAYs/MajorityElementMooresAlg.java
@@ -0,0 +1,30 @@
+
+class MajorityElementMooresAlg {
+
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+            // If the current number is the same as the candidate, increment count; otherwise, decrement count.
+            //normal code
+            // if (num == candidate) {
+            //     count++; 
+            // } else {
+            //     count--;
+            //}
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        MajorityElementMooresAlg solution = new MajorityElementMooresAlg();
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(solution.majorityElement(nums)); // Output: 2
+    }
+}
