f74bbe8821504eab925505f8a308ad69eb523fc7
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-06-05 22:26:00 +0530

    add

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,13 +4,16 @@ public class BinarySearch {
     public static int binarySearch(int nums[], int target) {
         // Binary Search works on sorted arrays
         // Time Complexity: O(log n)
+        // Space Complexity: O(1)
         int start = 0, end = nums.length - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (nums[mid] == target) {
+
                 return mid;
             } else if (nums[mid] < target) {
                 start = mid + 1;
+
             } else {
                 end = mid - 1;
             }
