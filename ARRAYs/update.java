7efa221fcdb3fb0cffcc42f16f2ac5aa395aab3e
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-06-05 22:24:47 +0530

    add

--- ARRAYs/update.java
+++ ARRAYs/update.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 
-public class update {
+public class update {// update the array
 
     public static void updateArray(int marks[]) {
         for (int i = 0; i < marks.length; i++) {
