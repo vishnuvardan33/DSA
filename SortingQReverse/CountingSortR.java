d897fc4c756978b59820040446c671253aa8a88e
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-06-08 16:00:43 +0530

    add

--- SortingQReverse/CountingSortR.java
+++ SortingQReverse/CountingSortR.java
@@ -1,31 +1,38 @@
+
 public class CountingSortR {
-	public static void sort(int arr[]){
-		int largest = Integer.MIN_VALUE;
-		for(int i=0; i<arr.length; i++){
-			largest = Math.max(largest, arr[i]);
-		}
-		int count[] = new int[largest+1];
-		for(int i=0; i<arr.length; i++){
-			count[arr[i]]++;
-		}
-		//sorting
-		int j=0;
-		for(int i=count.length-1; i>=0; i--){
-			while(count[i]>0){
-				arr[j]=i;
-				j++;
-				count[i]--;
-			}
-		}
-	}
-	public static void printarr(int arr[]){		
-		for(int i=0; i<arr.length; i++){
-			System.out.print(arr[i]+" ");
-		}
-	}
-	public static void main(String args[]){
-		int arr[]={1,4,0,2,3,1};
-		sort(arr);
-		printarr(arr);
-	}
+
+    public static void sort(int arr[]) {
+        //counting sort in reverse order
+        // Time Complexity: O(n+k) where k is the range of the input
+        // Space Complexity: O(k)
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        //sorting
+        int j = 0;
+        for (int i = count.length - 1; i >= 0; i--) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 4, 0, 2, 3, 1};
+        sort(arr);
+        printarr(arr);
+    }
 }
