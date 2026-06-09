fc64297d1b10bb647b6ca32571469728d9b37526
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-06-09 23:08:34 +0530

    add

--- conditional-statementsQ/leapYear.java
+++ conditional-statementsQ/leapYear.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class leapYear {
 
     public static void main(String[] args) {
+        // A leap year is a year that is divisible by 4 but not divisible by 100, or it is divisible by 400.
+        // Time Complexity: O(1)
+        // Space Complexity: O(1)
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a year:");
         int year = sc.nextInt();
