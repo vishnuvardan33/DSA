fc816959b4d8942336fd73efd564c8c76f1d4a6b
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-06-13 11:04:32 +0530

    add

--- ARRAYs/arrayTargetElement.java
+++ ARRAYs/arrayTargetElement.java
@@ -1,5 +1,9 @@
 
 public class arrayTargetElement {
+// Given an array of integers and a target value, return the index of the target value in the array. If the target value is not found in the array, return -1.
+// Example 1:
+// Input: nums = [1,2,3,4,5], target = 3
+// Output: 2
 
     public static int targetElement(int nums[], int target) {
         for (int i = 0; i < nums.length; i++) {
