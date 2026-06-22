43f27376f5fbadd93aab1e8df4780f02ff1abe16
D Vishnuvardan <dvishnuvardan33@gmail.com>
2026-06-22 07:51:52 +0530

    add

--- pattern/halfPyramid.java
+++ pattern/halfPyramid.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class halfPyramid {
 
     public static void main(String[] args) {
+        // This program prints a half pyramid pattern of numbers based on user input for the number of rows.
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int rows = sc.nextInt();
